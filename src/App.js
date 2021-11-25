ee060fc7b0a03e11afb96db2bc65c71c256da96e
ROBYN13Y <yrobyn3@gmail.com>
2021-11-24 23:26:39 -0800

    IMPORT
    importing project from codesandbox
    cleaning up to make it work

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./styles/App.css";
 
 import CitySearch from "./CitySearch";
-import CityWeatherInfo from "./CityWeatherInfo";
 
 function App() {
   return (
     <div className="container container-weather-app">
       <CitySearch />
-      <CityWeatherInfo />
     </div>
   );
 }
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./styles/CitySearch.css";
 
 export default function CitySearch() {
-  return (
+  let [city, setCity] = useState("");
+  let [result, setResult] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  let form = (
     <div className="CitySearch">
-      <form id="city-search-form">
+      <form id="city-search-form" onSubmit={citySubmit}>
         <label for="city-search-input" className="form-label" id="main-label">
           <span className="title-emoji">🌈</span> What is the weather?
         </label>
@@ -16,6 +21,7 @@ export default function CitySearch() {
           placeholder="TYPE CITY HERE!"
           size="34"
           id="city-search-input"
+          onChange={searchCity}
         />
         <input type="submit" className="city-search-button" value="SEARCH" />
         <input
@@ -26,4 +32,65 @@ export default function CitySearch() {
       </form>
     </div>
   );
+
+  function citySubmit(event) {
+    event.preventDefault();
+    let apiKey = "abaa5f9b2fdbce9c30bf8c70c78486ba";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function showWeather(response) {
+    setResult(true);
+    setWeather({
+      name: response.data.name,
+      temp: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function searchCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  if (result) {
+    return (
+      <div>
+        {form}
+        <div className="CurrentCity">
+          <div className="row">
+            <div className="col" id="col-text">
+              <h2 id="input-city-h2">{weather.name}</h2>
+              <h5 id="current-day-time-h5">Day of the Week, Time (hrs:mins)</h5>
+              <h6 id="month-day-year-h6">Month Day, Year</h6>
+              <h6 id="temp-description-h6">{weather.description} </h6>
+              <h6 id="wind-speed-h6">{weather.wind} (km/hr)</h6>
+            </div>
+            <div className="col" id="col-emoji">
+              <img
+                src={weather.icon}
+                alt="Current City Weather Icon"
+                width="110"
+                id="today-weather-icon"
+              />
+              <h6 id="city-temp-text">
+                {weather.temp}
+                <a href="/" id="c">
+                  °C
+                </a>
+                <a href="/" id="f">
+                  °F
+                </a>
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
